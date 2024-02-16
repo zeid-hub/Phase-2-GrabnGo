d369b82c2f3caa89a231ff8d8c589ffe896c38b2
zeid <zeidhassan4@gmail.com>
2024-02-16 13:09:11 +0300

    Done

--- src/Order.js
+++ src/Order.js
@@ -1,4 +1,4 @@
-// Order.js
+// Order.js (continued)
 import React, { useEffect, useState, useContext } from "react";
 import { CartContext } from "./CartProvider";
 import "./Order.css";
@@ -15,13 +15,42 @@ function Order() {
 
   return (
     <div className="container">
-      {/* ... your existing Order component */}
+      <h1 className="header">
+        OUR <span className="span">MENU</span>
+      </h1>
+      <div className="inner-container">
+        <p className="Parag">
+          Explore our extensive <span className="highlight">menu</span> together with us, featuring a wide range of snacks to satisfy every craving. With just a few taps, you can select your desired items, customize your order, and proceed to checkout, all without leaving
+          the comfort of your home.
+        </p>
+      </div>
+      <ul className="list">
+        {orderList.map((item) => (
+          <li key={item.id}>
+            <h2 className="inner-header">
+              <span className="name">Name:</span> {item.title}
+            </h2>
 
-      <button onClick={() => addToCart(item)} className="button">
-        Add To Cart
-      </button>
-
-      {/* ... rest of your component */}
+            <img className="image" src={item.image} alt={item.title} />
+            <h4 className="quantity">
+              Quantity:
+              <input
+                type="number"
+                value={item.qnty}
+                onChange={(e) => {
+                  console.log(e.target.value);
+                }}
+              />
+            </h4>
+            <h3 className="price">Price: {item.price}$</h3>
+            <button onClick={() => addToCart(item)} className="button">
+              Add To Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+      {/* Include Cart component to display the cart */}
+      {/* <Cart /> */}
     </div>
   );
 }
